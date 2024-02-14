0cc4816ee2717cac81dadf681c24987d9f990735
Yuanke <yuankemiao.dev@gmail.com>
2024-02-14 13:44:15 +0200

    basic router is working, might add more pages in the future

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./pages/Layout";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import Register from "./pages/Register";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="product/:productId" element={<ProductPage />} />
+          <Route path="profile/:userId" element={<Register />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
